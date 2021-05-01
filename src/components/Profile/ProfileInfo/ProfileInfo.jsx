509281f8659be886f65796c551fe7981898b81b1
Andrew <andrewphilimonchik@gmail.com>
2021-05-01 11:01:51 +0300

    episode 99

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,9 +5,12 @@ import ProfileStatus from './ProfileStatus';
 import userPhoto from '../../../assets/img/user-logo.png';
 
 const ProfileInfo = (props) => {
+
     if (!props.profile) {
         return <Preloader />
     };
+
+    // Contacts veriables
     let aboutMe = props.profile.aboutMe
     let FaceBookLink = props.profile.contacts.facebook;
     let GitHubLink = props.profile.contacts.github;
@@ -29,22 +32,21 @@ const ProfileInfo = (props) => {
                     <img className={s.profile_img} src={props.profile.photos.large !== null ? props.profile.photos.large : userPhoto} />
                 </div>
                 <div className={s.person_discription}>
-                    <ProfileStatus isOwner={props.isOwner} status={props.status} updateStatus={props.updateStatus} />
-                    <div className={s.discription}>
-                        <div><b>Job Status: </b>{props.profile.lookingForAJob ? 'Yes' : 'No'}</div>
-                        {props.profile.lookingForAJob && 
-                        <div><b>My professional skills: </b>{props.profile.lookingForAJobDiscription}</div>}
-                        <div><b>About Me: </b>{aboutMe ? aboutMe : 'no information'}</div>
-                        <div><b>Facebook: </b>{FaceBookLink ? FaceBookLink : 'no information'}</div>
-                        <div><b>GitHub: </b>{GitHubLink ? GitHubLink : 'no information'}</div>
-                        <div><b>Instagram: </b>{InstagramLink ? InstagramLink : 'no information'}</div>
-                        <div><b>VK: </b>{VkLink ? VkLink : 'no information'}</div>
+                    <ProfileStatus isOwner={props.isOwner} status={props.status} updateStatus={props.updateStatus} /> 
+                    <div className={s.discription}> 
+                        <div>Job Status: {props.profile.lookingForAJob ? 'Yes' : 'No'}</div>
+                        {props.profile.lookingForAJob && <div>My professional skills: {props.profile.lookingForAJobDiscription}</div>}
+                        <div>About Me: {aboutMe}</div>
+                        <div>Facebook: {FaceBookLink ? FaceBookLink : 'no info'}</div>
+                        <div>GitHub: {GitHubLink ? GitHubLink : 'no info'}</div>
+                        <div>Instagram: {InstagramLink ? InstagramLink : 'no info'}</div>
+                        <div>VK: {VkLink ? VkLink : 'no info'}</div>
                     </div>
-                    <div><b>{'Upload photo: '}</b>{props.isOwner && <input className={s.uploadPhoto} onChange={ onMainPhotoSelected } type={'file'}/>}</div>
+                    <div>{props.isOwner && <input className={s.uploadPhoto} onChange={ onMainPhotoSelected } type={'file'}/>}</div>
                 </div>
             </div>
         </div>
     )
-}
+};
 
 export default ProfileInfo;
